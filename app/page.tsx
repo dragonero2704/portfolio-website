ddd0eb6d6f258ae8684e5819839ae42999746370
dragonero2704 <rudi.digitale04@gmail.com>
2025-03-25 17:02:02 +0100

    moved components folder out of the apps folder & definition of Github Repo interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Metadata } from "next";
-import Nossr from "./components/nossr";
-import ScrambleText from "./components/animation/ScrambleText";
-import Cycle from "./components/animation/CycleText";
+import Nossr from "../components/nossr";
+import ScrambleText from "../components/animation/ScrambleText";
+import Cycle from "../components/animation/CycleText";
 
 export const metadata: Metadata = {
   title: "Rudidigital",
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,18 +1,25 @@
 import { Metadata } from "next";
 import styles from "./page.module.css";
-import ScrambleText from "../components/animation/ScrambleText";
+import ScrambleText from "../../components/animation/ScrambleText";
+import ListRepos from "../../components/github/ListRepos";
 
 export const metadata: Metadata = {
   title: "Projects",
   // icons: "../favicon.ico",
 };
 
-export default function Page(props) {
-
-
+export default async function Page(props) {
+  const repos = await ListRepos();
   return (
-    <h1>
-      <ScrambleText>Projects</ScrambleText>
-    </h1>
+    <>
+      <h1>
+        <ScrambleText>Projects</ScrambleText>
+      </h1>
+      <>
+        {repos.map((repo) => (
+          <p>{repo.name} <span>{repo.visibility}</span> <span>{repo.languages_url}</span></p>
+        ))}
+      </>
+    </>
   );
 }
